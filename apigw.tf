caf22d81b88b963a7916833df40fda2a16e517d6
PoliuhovychMax <maksym.poliukhovych.ri.2025@lpnu.ua>
2026-04-07 01:54:24 +0200

    lambda (fixed again)

--- apigw.tf
+++ apigw.tf
@@ -109,3 +109,60 @@ resource "aws_api_gateway_deployment" "deployment" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_method" "options" {
+  for_each = local.resource_map
+
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = each.value
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options" {
+  for_each = local.resource_map
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = each.value
+  http_method = aws_api_gateway_method.options[each.key].http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options" {
+  for_each = local.resource_map
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = each.value
+  http_method = aws_api_gateway_method.options[each.key].http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options" {
+  for_each = local.resource_map
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = each.value
+  http_method = aws_api_gateway_method.options[each.key].http_method
+  status_code = aws_api_gateway_method_response.options[each.key].status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
